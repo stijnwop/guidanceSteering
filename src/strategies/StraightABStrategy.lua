9a9316ee5d2ff96041d8ff99523f8ea9d21fd299
Stijn Wopereis <stijnwop@gmail.com>
2018-09-07 23:11:39 +0200

    Move line drawing to strategy including target line arrow

--- src/strategies/StraightABStrategy.lua
+++ src/strategies/StraightABStrategy.lua
@@ -1,5 +1,19 @@
 StraightABStrategy = {}
 
+local ABLine = {
+    ["middle"] = { position = 0, rgb = { 1, 1, 1 } }
+}
+
+local ABLines = {
+    ["left"] = { position = -1, rgb = { 0, 0, .7 } },
+    ["middle"] = { position = 0, rgb = { 1, 1, 1 } },
+    ["right"] = { position = 1, rgb = { 0, 0, .7 } },
+}
+
+StraightABStrategy.STEP_SIZE = 1 -- 1m each line
+StraightABStrategy.NUM_STEPS = 15 -- draw 15
+StraightABStrategy.GROUND_CLEARANCE_OFFSET = .2
+
 local StraightABStrategy_mt = Class(StraightABStrategy)
 
 function StraightABStrategy:new(customMt)
@@ -27,7 +41,44 @@ function StraightABStrategy:update(dt)
     end
 end
 
-function StraightABStrategy:draw(info)
+function StraightABStrategy:draw(data)
+    local lines = ABLine
+    local drawBotherLines = self:getIsGuidancesPossible()
+    local x, _, z, lineDirX, lineDirZ = unpack(data.driveTarget)
+
+    if drawBotherLines then
+        lineDirX, lineDirZ = unpack(data.snapDirection)
+        lines = ABLines
+    end
+
+    local lineXDir = data.snapDirectionMultiplier * lineDirX * data.movingDirection
+    local lineZDir = data.snapDirectionMultiplier * lineDirZ * data.movingDirection
+
+    for _, line in pairs(lines) do
+        local lineX = x + data.width * lineDirZ * (data.alphaRad + line.position / 2)
+        local lineZ = z - data.width * lineDirX * (data.alphaRad + line.position / 2)
+
+        local r, g, b = unpack(line.rgb)
+
+        for l = 1, StraightABStrategy.NUM_STEPS do
+            local x1 = lineX + StraightABStrategy.STEP_SIZE * l * lineXDir
+            local z1 = lineZ + StraightABStrategy.STEP_SIZE * l * lineZDir
+            local y1 = getTerrainHeightAtWorldPos(g_currentMission.terrainRootNode, x1, 0, z1) + StraightABStrategy.GROUND_CLEARANCE_OFFSET
+            local x2 = lineX + StraightABStrategy.STEP_SIZE * (l + 1) * lineXDir
+            local z2 = lineZ + StraightABStrategy.STEP_SIZE * (l + 1) * lineZDir
+            local y2 = getTerrainHeightAtWorldPos(g_currentMission.terrainRootNode, x2, 0, z2) + StraightABStrategy.GROUND_CLEARANCE_OFFSET
+
+            drawDebugLine(x1, y1, z1, r, g, b, x2, y2, z2, r, g, b)
+        end
+
+        -- draw direction arrow
+        if line.position == 0 then
+            local x = lineX + StraightABStrategy.STEP_SIZE * StraightABStrategy.NUM_STEPS * lineXDir
+            local z = lineZ + StraightABStrategy.STEP_SIZE * StraightABStrategy.NUM_STEPS * lineZDir
+            local y = getTerrainHeightAtWorldPos(g_currentMission.terrainRootNode, x, 0, z) + StraightABStrategy.GROUND_CLEARANCE_OFFSET
+            drawDebugArrow(x, y, z, lineXDir, 0, lineZDir, 0, math.tan(90), 0, r, g, b)
+        end
+    end
 end
 
 function StraightABStrategy:handleABPoints(guidanceNode, data)
