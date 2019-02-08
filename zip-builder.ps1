b2c3d5d1a099ae145164efe1a939346dc5f949f4
Stijn Wopereis <stijnwop@gmail.com>
2019-02-08 22:28:40 +0100

    Use 7zip or Winrar to compress, FS does not support .NET zip entry compression. (#75)

--- zip-builder.ps1
+++ zip-builder.ps1
@@ -1,6 +1,12 @@
-$exclude = @(".idea", ".github", ".gitignore", "zip.bat", "zip-builder.ps1", ".git")
-$path = $(get-location).Path;
-$destination = $path + "\FS19_guidanceSteering_dev.zip"
-$files = Get-ChildItem -Path $path -Exclude $exclude
+$include = @("*.xml", "*.lua", "*.i3d", "*.i3d.shapes", "*.dds", "*.ogg", "README.md")
+$zipfilename = "FS19_guidanceSteering_dev.zip"
 
-Compress-Archive -Path $files -DestinationPath $destination -CompressionLevel Fastest
+if (test-path "$env:ProgramFiles\7-Zip\7z.exe") {
+	set-alias 7z "$env:ProgramFiles\7-Zip\7z.exe"
+	7z a -tzip $zipfilename $include -r -xr!".idea" -xr!"data"
+} elseif (test-path "$env:ProgramFiles\WinRAR\WinRAR.exe") {
+	set-alias winrar "$env:ProgramFiles\WinRAR\WinRAR.exe"
+	winrar a -afzip $zipfilename $include -r -x".idea" -x"data"
+} else {
+	throw "7-Zip or WinRAR is needed!"
+}
--- zip.bat
+++ zip.bat
@@ -1,2 +1,3 @@
 del FS19_guidanceSteering_dev.zip
 powershell -executionpolicy remotesigned -File  zip-builder.ps1
+pause
