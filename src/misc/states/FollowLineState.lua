b2043524cda0e56458f78c2837997687f310e5ef
stijnwop <stijnwop@gmail.com>
2019-02-13 22:43:00 +0100

    Introduce finite state machine to handle the guidance cycle

---
+++ src/misc/states/FollowLineState.lua
@@ -0,0 +1,32 @@
+---
+-- FollowLineState
+--
+-- Main state for guiding along a line.
+--
+-- Copyright (c) Wopster, 2019
+
+FollowLineState = {}
+
+local FollowLineState_mt = Class(FollowLineState)
+
+function FollowLineState:new(object, custom_mt)
+    local instance = {}
+
+    setmetatable(instance, custom_mt or FollowLineState_mt)
+
+    instance.object = object
+
+    return instance
+end
+
+function FollowLineState:update(dt)
+    local object = self.object
+    local spec = object:guidanceSteering_getSpecTable("globalPositioningSystem")
+
+    GlobalPositioningSystem.guideSteering(object, dt)
+    spec.headlandProcessor:handle(dt)
+
+    -- Todo: check if on headland then trigger next state.
+
+    Logger.info("Follow line")
+end
