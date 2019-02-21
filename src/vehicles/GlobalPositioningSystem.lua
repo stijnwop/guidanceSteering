50c18768af43360238c9f7e108b8886cb55fe5d6
stijnwop <stijnwop@gmail.com>
2019-02-21 21:24:32 +0100

    Move the guidance of the vehicle to the drive utility.

--- src/vehicles/GlobalPositioningSystem.lua
+++ src/vehicles/GlobalPositioningSystem.lua
@@ -168,13 +168,9 @@ function GlobalPositioningSystem:onLoad(savegame)
     spec.guidanceSteeringIsActive = false
     spec.guidanceTerrainAngleIsActive = true
     spec.autoInvertOffset = false
-
     spec.shiftParallel = false
 
     spec.abDistanceCounter = 0
-
-    -- Processor to handle actions on the headland
-    spec.headlandProcessor = HeadlandProcessor:new(self)
 
     spec.lastInputValues = {}
     spec.lastInputValues.guidanceIsActive = true
@@ -491,7 +487,7 @@ function GlobalPositioningSystem:onUpdate(dt)
 
     local drivingDirection = self:getDrivingDirection()
     local guidanceSteeringIsActive = spec.guidanceSteeringIsActive
-    local x, y, z, driveDirX, driveDirZ = unpack(data.driveTarget)
+    local x, _, z, driveDirX, driveDirZ = unpack(data.driveTarget)
 
     -- Only compute when the vehicle is moving
     if drivingDirection ~= 0 or spec.shiftParallel then
@@ -858,6 +854,7 @@ function GlobalPositioningSystem:onSteeringStateChanged(isActive)
 end
 
 function GlobalPositioningSystem:onHeadlandStart()
+    -- Todo: do we need it.
     if not self.isClient then
         return
     end
@@ -868,6 +865,7 @@ function GlobalPositioningSystem:onHeadlandStart()
 end
 
 function GlobalPositioningSystem:onHeadlandEnd()
+    -- Todo: do we need it.
     if not self.isClient then
         return
     end
