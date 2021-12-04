432815f2be63ceb5c418c27269088cf6b24c2733
Stijn Wopereis <stijn.wopereis@creative-mesh.com>
2021-12-04 16:57:53 +0100

    Port HUD to FS22

--- src/gui/hud/GuidanceSteeringHUD.lua
+++ src/gui/hud/GuidanceSteeringHUD.lua
@@ -43,11 +43,9 @@ end
 
 --- Create the elements for the HUD.
 function GuidanceSteeringHUD:createElements()
-    local rightX = 1 - g_safeFrameOffsetX -- right of screen.
-    local topRightX, topRightY = SpeedMeterDisplay.getBackgroundPosition(1)
+    local topRightX, topRightY = self.speedMeterDisplay.gearIcon:getPosition()
     local marginWidth, marginHeight = self.speedMeterDisplay:scalePixelToScreenVector(GuidanceSteeringHUD.SIZE.BOX_MARGIN)
-
-    local y = self:createBox(self.uiFilename, rightX, topRightY - marginHeight) - marginHeight
+    self:createBox(self.uiFilename, topRightX + marginWidth, topRightY + marginHeight)
 end
 
 --- Create the box with the HUD icons.
@@ -56,24 +54,27 @@ function GuidanceSteeringHUD:createBox(hudAtlasPath, x, y)
     local posX = x - boxWidth * 0.5
 
     local iconWidth, iconHeight = self.speedMeterDisplay:scalePixelToScreenVector(GuidanceSteeringHUD.SIZE.ICON)
+    local iconPosX, iconPosY = self.speedMeterDisplay:scalePixelToScreenVector(GuidanceSteeringHUD.POSITION.ICON)
 
-    local boxOverlay = Overlay:new(nil, posX, y, boxWidth, boxHeight)
-    local boxElement = HUDElement:new(boxOverlay)
+    local boxOverlay = Overlay.new(g_baseHUDFilename, posX, y, boxWidth, boxHeight)
+    local boxElement = HUDElement.new(boxOverlay)
     self.stateBox = boxElement
+
+    self.stateBox:setUVs(GuiUtils.getUVs(SpeedMeterDisplay.UV.GEARS_BAR))
+    self.stateBox:setColor(unpack(SpeedMeterDisplay.COLOR.GEARS_BG))
+
     self.stateBox:setVisible(true)
     self.speedMeterDisplay:addChild(boxElement)
 
-    self.steeringIcon = self:createIcon(hudAtlasPath, posX, y, iconWidth, iconHeight, GuidanceSteeringHUD.UV.STEERING_WHEEL_DISABLED)
+    self.steeringIcon = self:createIcon(hudAtlasPath, posX + iconPosX, y + iconPosY, iconWidth, iconHeight, GuidanceSteeringHUD.UV.STEERING_WHEEL_DISABLED)
 
     self.stateBox:addChild(self.steeringIcon)
+    self.steeringIcon:setColor(unpack(GuidanceSteeringHUD.COLOR.INACTIVE))
     self.steeringIcon:setVisible(true)
 
     y = y + iconHeight
 
-    local separator = self:createHorizontalSeparator(g_baseHUDFilename, posX, y)
-    self.stateBox:addChild(separator)
-
-    self.receiverIcon = self:createIcon(hudAtlasPath, posX, y, iconWidth, iconHeight, GuidanceSteeringHUD.UV.RECEIVER)
+    self.receiverIcon = self:createIcon(hudAtlasPath, posX + iconPosX, y + iconPosY, iconWidth, iconHeight, GuidanceSteeringHUD.UV.RECEIVER)
 
     self.stateBox:addChild(self.receiverIcon)
     self.receiverIcon:setColor(unpack(GuidanceSteeringHUD.COLOR.INACTIVE))
@@ -81,12 +82,10 @@ function GuidanceSteeringHUD:createBox(hudAtlasPath, x, y)
 
     y = y + iconHeight
 
-    separator = self:createHorizontalSeparator(g_baseHUDFilename, posX, y)
-    self.stateBox:addChild(separator)
-
-    self.laneIcon = self:createIcon(hudAtlasPath, posX, y, iconWidth, iconHeight, GuidanceSteeringHUD.UV.LANE)
+    self.laneIcon = self:createIcon(hudAtlasPath, posX + iconPosX, y + iconPosY, iconWidth, iconHeight, GuidanceSteeringHUD.UV.LANE)
 
     self.stateBox:addChild(self.laneIcon)
+    self.laneIcon:setColor(unpack(GuidanceSteeringHUD.COLOR.INACTIVE))
     self.laneIcon:setVisible(true)
 
     return x - boxWidth
@@ -94,24 +93,13 @@ end
 
 --- Create icon for the HUD.
 function GuidanceSteeringHUD:createIcon(imagePath, baseX, baseY, width, height, uvs)
-    local iconOverlay = Overlay:new(imagePath, baseX, baseY, width, height)
-    iconOverlay:setUVs(getNormalizedUVs(uvs))
-    local element = HUDElement:new(iconOverlay)
+    local iconOverlay = Overlay.new(imagePath, baseX, baseY, width, height)
+    iconOverlay:setUVs(GuiUtils.getUVs(uvs))
+    local element = HUDElement.new(iconOverlay)
 
     element:setVisible(false)
 
     return element
-end
-
---- Create a horizontal separator.
-function GuidanceSteeringHUD:createHorizontalSeparator(hudAtlasPath, baseX, baseY)
-    local posX, posY = getNormalizedScreenValues(unpack(GuidanceSteeringHUD.POSITION.SEPARATOR))
-    local width, height = getNormalizedScreenValues(unpack(GuidanceSteeringHUD.SIZE.SEPARATOR))
-    local separatorOverlay = Overlay:new(hudAtlasPath, baseX + posX, baseY + posY, width, height)
-    separatorOverlay:setUVs(getNormalizedUVs(SpeedMeterDisplay.UV.SEPARATOR))
-    separatorOverlay:setColor(unpack(SpeedMeterDisplay.COLOR.SEPARATOR))
-
-    return HUDElement:new(separatorOverlay)
 end
 
 function GuidanceSteeringHUD:storeScaledValues()
@@ -126,11 +114,6 @@ function GuidanceSteeringHUD:storeScaledValues()
     self.laneTextPositionX = boxPosX + boxWidth + textOffX
     self.laneTextPositionY = boxPosY + boxHeight + textOffY
     self.laneTextSize = self.speedMeterDisplay:scalePixelToScreenHeight(GuidanceSteeringHUD.TEXT_SIZE.LANE)
-
-    textOffX, textOffY = self.speedMeterDisplay:scalePixelToScreenVector(GuidanceSteeringHUD.POSITION.ANGLE_TEXT)
-    self.angleTextPositionX = boxPosX + boxWidth + textOffX
-    self.angleTextPositionY = boxPosY + textOffY
-    self.angleTextSize = self.speedMeterDisplay:scalePixelToScreenHeight(GuidanceSteeringHUD.TEXT_SIZE.ANGLE)
 end
 
 --- Sets the current vehicle to display on the HUD.
@@ -160,20 +143,26 @@ function GuidanceSteeringHUD:drawText()
         self.laneText = self:getLaneText(data.currentLane)
         if self.steeringIconIsActive ~= spec.guidanceSteeringIsActive then
             self.steeringIconIsActive = spec.guidanceSteeringIsActive
-            local uvs = spec.guidanceSteeringIsActive and GuidanceSteeringHUD.UV.STEERING_WHEEL_ENABLED or GuidanceSteeringHUD.UV.STEERING_WHEEL_DISABLED
-            local color = spec.guidanceSteeringIsActive and GuidanceSteeringHUD.COLOR.ACTIVE or GuidanceSteeringHUD.COLOR.INACTIVE
-            self.steeringIcon:setUVs(getNormalizedUVs(uvs))
+            local uvs = self.steeringIconIsActive and GuidanceSteeringHUD.UV.STEERING_WHEEL_ENABLED or GuidanceSteeringHUD.UV.STEERING_WHEEL_DISABLED
+            local color = self.steeringIconIsActive and GuidanceSteeringHUD.COLOR.ACTIVE or GuidanceSteeringHUD.COLOR.INACTIVE
+
+            self.steeringIcon:setUVs(GuiUtils.getUVs(uvs))
             self.steeringIcon:setColor(unpack(color))
         end
 
         if self.receiverIconIsActive ~= spec.guidanceIsActive then
             self.receiverIconIsActive = spec.guidanceIsActive
-            local color = spec.guidanceIsActive and GuidanceSteeringHUD.COLOR.ACTIVE or GuidanceSteeringHUD.COLOR.INACTIVE
+            local color = self.receiverIconIsActive and GuidanceSteeringHUD.COLOR.ACTIVE or GuidanceSteeringHUD.COLOR.INACTIVE
+
             self.receiverIcon:setColor(unpack(color))
+            self.laneIcon:setColor(unpack(color))
         end
 
         self:drawLaneText()
-        self:drawDirectionAngleText(spec.guidanceNode)
+
+        local topRightX, topRightY = self.speedMeterDisplay.gearIcon:getPosition()
+        local marginWidth, marginHeight = self.speedMeterDisplay:scalePixelToScreenVector(GuidanceSteeringHUD.SIZE.BOX_MARGIN)
+        self.stateBox:setPosition(topRightX + marginWidth, topRightY + marginHeight)
 
         setTextBold(false)
         setTextAlignment(RenderText.ALIGN_LEFT)
@@ -181,40 +170,29 @@ function GuidanceSteeringHUD:drawText()
 end
 
 function GuidanceSteeringHUD:drawLaneText()
+    local color = self.receiverIconIsActive and GuidanceSteeringHUD.COLOR.ACTIVE or GuidanceSteeringHUD.COLOR.INACTIVE
+
     setTextBold(true)
     setTextAlignment(RenderText.ALIGN_RIGHT)
-    setTextColor(unpack(GuidanceSteeringHUD.TEXT_COLOR.LANE))
+    setTextColor(unpack(color))
 
     if self.laneTextPositionX ~= nil then
         renderText(self.laneTextPositionX, self.laneTextPositionY, self.laneTextSize, self.laneText)
     end
 end
 
-function GuidanceSteeringHUD:drawDirectionAngleText(node)
-    setTextBold(true)
-    setTextAlignment(RenderText.ALIGN_RIGHT)
-    setTextColor(unpack(GuidanceSteeringHUD.TEXT_COLOR.LANE))
-
-    local dirX, _, dirZ = localDirectionToWorld(node, 0, 0, 1)
-    local angleRad = math.abs(MathUtil.getYRotationFromDirection(dirX, dirZ) - math.pi)
-    if self.angleTextPositionX ~= nil then
-        renderText(self.angleTextPositionX, self.angleTextPositionY, self.angleTextSize, tostring(MathUtil.round(math.deg(angleRad), 1)) .. "°")
-    end
-end
-
 function GuidanceSteeringHUD.speedMeterDisplay_storeScaledValues(speedMeterDisplay)
-    g_guidanceSteering.ui.hud:storeScaledValues()
+    g_currentMission.guidanceSteering.ui.hud:storeScaledValues()
 end
 
 function GuidanceSteeringHUD.speedMeterDisplay_draw(speedMeterDisplay)
-    g_guidanceSteering.ui.hud:drawText()
+    g_currentMission.guidanceSteering.ui.hud:drawText()
 end
 
 GuidanceSteeringHUD.SIZE = {
-    BOX = { 54, 162 },
-    BOX_MARGIN = { 11, 44 },
-    ICON = { 54, 54 },
-    SEPARATOR = { 54, 1 },
+    BOX = { 44, 110 },
+    BOX_MARGIN = { -5, 50 },
+    ICON = { 32, 32 },
 }
 GuidanceSteeringHUD.UV = {
     STEERING_WHEEL_ENABLED = { 650, 0, 65, 65 },
@@ -224,23 +202,19 @@ GuidanceSteeringHUD.UV = {
 }
 
 GuidanceSteeringHUD.POSITION = {
-    WIDTH_TEXT = { 30, -62.5 },
-    WIDTH_ICON = { 20, 25 },
-    SEPARATOR = { 0, 0 },
-    LANE_TEXT = { -8, -20 },
-    ANGLE_TEXT = { -8, -8 },
+    LANE_TEXT = { -7, -20 },
+    ICON = { 5, 5 },
 }
 
 GuidanceSteeringHUD.TEXT_COLOR = {
-    LANE = { 1, 1, 1, 1 }
+    LANE = { 0.7, 0.7, 0.7, 0.3 }
 }
 
 GuidanceSteeringHUD.COLOR = {
-    INACTIVE = { 1, 1, 1, 0.75 },
-    ACTIVE = { 0.0953, 1, 0.0685, 0.75 }
+    INACTIVE = { 0.7, 0.7, 0.7, 0.3 },
+    ACTIVE = { 0.0003, 0.5647, 0.9822, 1 }
 }
 
 GuidanceSteeringHUD.TEXT_SIZE = {
-    LANE = 14,
-    ANGLE = 12
+    LANE = 14
 }
